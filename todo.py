76e9e72dbdc601731048ad4107e872453abd2ec8
Xu Tian <tianxu03@gmail.com>
2017-01-05 11:17:16 -0500

    update

---
+++ todo.py
@@ -0,0 +1,48 @@
+# todo.py
+
+# todo
+# http://www.in2.nyc/press/
+# https://mp.weixin.qq.com/s?__biz=MzAxMTY0ODUwNQ==&mid=2649781447&idx=1&sn=ccf3376f5b5eed6dde852cd1b06ee0f3&chksm=83b93851b4ceb147531b6d85da187092f2545b3178e59c84f7cec2e398573faec6327dc85415&mpshare=1&scene=1&srcid=1228IJycNjAYr6MI2GJQmHfn&key=564c3e9811aee0abc5fc58bd02a1e40b3496f245653ff573686bc81ac3b4e697e06f76cea11294ac1e25b29c5622d4f96e3daade0c4b33a5fe38ffa508a7a38f7dcf95d749e232b7ae00147c5c5f9c32&ascene=0&uin=MjU1ODk4MTE4Mw%3D%3D&devicetype=iMac+MacBookPro11%2C2+OSX+OSX+10.11.6+build(15G1212)&version=12010210&nettype=WIFI&fontScale=100&pass_ticket=uvXfFrBuAWffUgb%2BPaUlweumvqaIjTBBwKb7CljcrJHqNDno6dT3olQ5WFqibZBi
+# https://mp.weixin.qq.com/s?__biz=MjM5MTQzNzU2NA==&mid=2651646706&idx=2&sn=945de646f8c2190ab81323289c48d1a1&chksm=bd4dcd618a3a447789b318a0a7bef0620a9ba0a33b40a2d629898643d6173e7c3de4a9ef15d3&scene=0&key=564c3e9811aee0ab873575f00237ef582da08fb1d9e0940bc5aea5089ffd8ca1c95e3b981a27eb25e6622cb0778495772909c477b26701817c620874bdd6c55c7ab1bbe2093804059cb9597cf93c48c2&ascene=0&uin=MjU1ODk4MTE4Mw%3D%3D&devicetype=iMac+MacBookPro11%2C2+OSX+OSX+10.11.6+build(15G1212)&version=12010210&nettype=WIFI&fontScale=100&pass_ticket=uvXfFrBuAWffUgb%2BPaUlweumvqaIjTBBwKb7CljcrJHqNDno6dT3olQ5WFqibZBi
+# https://mp.weixin.qq.com/s?__biz=MjM5MTQzNzU2NA==&mid=2651646706&idx=1&sn=14595524b7ae9b0cefcfb3f0cedfae92&chksm=bd4dcd618a3a44772ad771eda8236532a854e2198ffba51e137469dd67ba54886d4a0c4549d2&scene=0&key=564c3e9811aee0abbd1f7a68f9a2fceb44cf9b5584a4bb0650bd83260df9d8577715634157abfe431d2cade094b9489d4cb4b3e5cb0951f5ee3987d8dd856c29b1157dc97f74c03a7bfdb42fa725ad92&ascene=0&uin=MjU1ODk4MTE4Mw%3D%3D&devicetype=iMac+MacBookPro11%2C2+OSX+OSX+10.11.6+build(15G1212)&version=12010210&nettype=WIFI&fontScale=100&pass_ticket=uvXfFrBuAWffUgb%2BPaUlweumvqaIjTBBwKb7CljcrJHqNDno6dT3olQ5WFqibZBi
+
+# http://code.activestate.com/recipes/577197-sortedcollection/
+# https://www.linkedin.com/pulse/11-strategic-ways-speedup-r-code-saranya-anandh?trk=hp-feed-article-title-like
+# https://www.kaggle.com/c/two-sigma-financial-modeling
+# https://www.bloomberg.com/news/articles/2016-11-10/hedge-funds-beware-most-machine-learning-talk-is-really-hokum
+# https://www.bloomberg.com/news/articles/2016-11-22/big-data-is-a-big-mess-for-hedge-funds-hunting-for-trade-signals
+# http://mp.weixin.qq.com/s?__biz=MzI0ODAwODMzNg==&mid=2650570038&idx=1&sn=3b0e9de9b44400f1b93e29d93f896fc7&chksm=f1af86c9c6d80fdf0d9a2b3bba001da74e38419b4869ae5be5ac0e50c241d6a4dc474da4b444&scene=0#rd
+# https://mp.weixin.qq.com/s?__biz=MjM5NDk0ODEwNA==&mid=2650333202&idx=2&sn=65c50033a3fab7fedaa98b82f8448a0e&key=fe4410a4c5efc39c794b7f45bd93e7509db11c77489807131e707cdc22544896d600738bc982557e7a63219a76d5f00d&ascene=0&uin=MjU1ODk4MTE4Mw%3D%3D&devicetype=iMac+MacBookPro11%2C2+OSX+OSX+10.11.6+build(15G1108)&version=12000510&nettype=WIFI&fontScale=100&pass_ticket=ZiKSziw1IAPdohxpoHdka9nt28J9d0k0UN%2F7q28v0JDKaIFsTzQkCw8Tz%2BaafZni
+# https://github.com/seblau/Roulette-Simulator
+# https://julien.danjou.info/blog/2013/guide-python-static-class-abstract-methods
+# https://en.wikipedia.org/wiki/Bubble_sort
+# https://en.wikipedia.org/wiki/Heapsort
+# https://en.wikipedia.org/wiki/Selection_sort
+# https://classroom.udacity.com/courses/ud513/lessons/7174469398/concepts/71212749920923
+# http://posts.careerengine.us/p/56a7ec00a3522f4843eab68e?from=latest-posts-panel&type=previewimage
+# http://faculty.chicagobooth.edu/ruey.tsay/teaching/uts/
+# https://www.coursera.org/payments/checkout?cartId=6661287
+
+
+# To study
+# https://zhuanlan.zhihu.com/p/24536868?hmsr=toutiao.io&utm_medium=toutiao.io&utm_source=toutiao.io
+
+# https://github.com/ZuzooVn/machine-learning-for-software-engineers/blob/master/README.md
+
+# https://github.com/jwasham/google-interview-university
+
+# Vatic Labs https://docs.google.com/document/d/1bzZUigCpwzWZncArt4e1ri2qAIw2ScAVM2QXtmQvcE4/edit
+
+## Jobs
+# http://enigma.io/position-detail/?id=61090
+# https://boards.greenhouse.io/enigmaio/jobs/61090#.WGVOWLYrK-Y
+
+## Adv Books
+# https://wiki.python.org/moin/AdvancedBooks
+# https://www.fullstackpython.com/best-python-resources.html
+# https://doughellmann.com/blog/the-python-standard-library-by-example/
+# PyMOTW: https://pymotw.com/2/contents.html
+# PyMOTW: https://pymotw.com/2/collections/index.html
+# http://planspace.org/20150111-a_great_python_book_explains_hash_tables/
+
+
