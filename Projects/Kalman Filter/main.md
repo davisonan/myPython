3e0db412fbb97f1e6fd38ba35c5c31f055a2ecfd
Xu Tian <tianxu03@gmail.com>
2017-03-12 15:35:11 -0400

    update

---
+++ Projects/Kalman Filter/main.md
@@ -0,0 +1,4 @@
+# Kalman Filter
+
+This is perhaps the first time domain filtering technique Hal ever showed me based on the paper Understanding the Kalman Filter (1983).
+
---
+++ Projects/SA/statisticalArbitrage.py
@@ -0,0 +1,15 @@
+# statisticalArbitrage.py
+
+#' # One working example of the statistical arbitrage strategy
+#' Statistical arbitrage is a well-known strategy that has been 
+#' in the market for a long time. At least to me, when I was still
+#' in graduate school a long time ago, I once read lecture slides 
+#' online about this strategy, I've already got an impression that
+#' this strategy at least had been successfully implemented, otherwise
+#' people wouldn't talk about it. After these many years, some strategies
+#' are still working, some are not, even so, it's still worth time working
+#' through one example to get a flavour of this once-famous strategy. Here 
+#' we go!
+
+#' # Introduction
+#' Statistical arbitrage (SA) is a strategy exploring the blahblah...
