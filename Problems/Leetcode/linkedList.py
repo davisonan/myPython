8de1897579d78c5ea6dda5b2a1e3566f9ef5cb05
Xu Tian <tianxu03@gmail.com>
2017-03-20 14:15:15 -0400

    update

---
+++ Problems/Leetcode/linkedList.py
@@ -0,0 +1,7 @@
+#' % Problems related to linked lists
+#' % Xu Tian
+#' % 2017/03/20
+
+#' # Background
+#' This writeup consists of the tree-based problems in leetcode. 
+
---
+++ Problems/Leetcode/tree.py
@@ -0,0 +1,7 @@
+#' % Problems related to binary search tree
+#' % Xu Tian
+#' % 2017/03/17
+
+#' # Background
+#' This writeup consists of the tree-based problems in leetcode. 
+
