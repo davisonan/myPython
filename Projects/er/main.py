e9af6959d18ad5027f30ba8db7cabb0b18de83a7
Xu Tian <tianxu03@gmail.com>
2017-04-19 10:28:22 -0400

    update

---
+++ Projects/er/main.py
@@ -0,0 +1,98 @@
+# main.py
+
+import requests
+import urllib.request
+import re
+from bs4 import BeautifulSoup
+
+site= "http://www.nasdaq.com/earnings/report/acrx"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8'}
+
+url = "http://biz.yahoo.com/research/earncal/20150309.html"
+
+content = urllib.request.urlopen(url).read()
+
+soup = BeautifulSoup(content)
+
+m = re.findall('center><small>\S+ ', content)
+w = re.findall('\?s=\w+',content)
+
+x=0
+lp = (len(m))
+xlp = lp -1
+
+
+for x in range (xlp):
+    print x, m[x+1][14:], w[x][3:]
+
+
+
+#!/usr/bin/env python
+
+import urllib.request
+import pytz
+import pandas as pd
+
+from bs4 import BeautifulSoup
+from datetime import datetime
+from pandas.io.data import DataReader
+
+
+SITE = "http://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+START = datetime(1900, 1, 1, 0, 0, 0, 0, pytz.utc)
+END = datetime.today().utcnow()
+
+
+def scrape_list(site):
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = urllib2.Request(site, headers=hdr)
+    page = urllib2.urlopen(req)
+    soup = BeautifulSoup(page)
+
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    sector_tickers = dict()
+    for row in table.findAll('tr'):
+        col = row.findAll('td')
+        if len(col) > 0:
+            sector = str(col[3].string.strip()).lower().replace(' ', '_')
+            ticker = str(col[0].string.strip())
+            if sector not in sector_tickers:
+                sector_tickers[sector] = list()
+            sector_tickers[sector].append(ticker)
+    return sector_tickers
+
+
+def download_ohlc(sector_tickers, start, end):
+    sector_ohlc = {}
+    for sector, tickers in sector_tickers.iteritems():
+        print 'Downloading data from Yahoo for %s sector' % sector
+        data = DataReader(tickers, 'yahoo', start, end)
+        for item in ['Open', 'High', 'Low']:
+            data[item] = data[item] * data['Adj Close'] / data['Close']
+        data.rename(items={'Open': 'open', 'High': 'high', 'Low': 'low',
+                           'Adj Close': 'close', 'Volume': 'volume'},
+                    inplace=True)
+        data.drop(['Close'], inplace=True)
+        sector_ohlc[sector] = data
+    print 'Finished downloading data'
+    return sector_ohlc
+
+
+def store_HDF5(sector_ohlc, path):
+    with pd.get_store(path) as store:
+        for sector, ohlc in sector_ohlc.iteritems():
+            store[sector] = ohlc
+
+
+def get_snp500():
+    sector_tickers = scrape_list(SITE)
+    sector_ohlc = download_ohlc(sector_tickers, START, END)
+    store_HDF5(sector_ohlc, 'snp500.h5')
+
+
+if __name__ == '__main__':
+    get_snp500()
